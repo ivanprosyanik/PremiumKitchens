31946929a0c809173e11bd373b9fee65bfcc8c31
Ivan Prosyanik <vanyaprosyanik89@gmail.com>
2023-07-08 17:26:45 +0300

    make responsive for 1-st screeen without footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,63 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper('.portfolio__content', {
+  const portfolioSlider = new Swiper('.portfolio__content', {
     slidesPerView: 'auto',
     spaceBetween: 42,
+    pagination: {
+      el: ".portfolio__dots",
+      bulletClass: "portfolio__dot",
+      bulletActiveClass: "portfolio__dot--active",
+      clickable: true,
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        slidesPerGroup: 1,
+        centeredSlides: true,
+        spaceBetween: 20,
+      },
+
+      768: {
+        slidesPerView: 'auto',
+        spaceBetween: 30,
+      },
+
+      992: {
+        slidesPerView: 'auto',
+        spaceBetween: 42,
+      }
+    }
   });
+
+  const burger = document.querySelector('.burger');
+  const headerList = document.querySelector('.header__list');
+  const body = document.querySelector('body')
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('burger--active');
+    headerList.classList.toggle('active');
+    body.classList.toggle('lock');
+  });
+
+  document.addEventListener('click', function (e) {
+    if (e.target !== burger && e.target !== headerList) {
+      body.classList.remove('lock');
+      headerList.classList.remove('active');
+      burger.classList.remove('burger--active');
+    };
+  });
+
+  if (window.innerWidth < 577) {
+    const materialsSwiper = new Swiper('.materials__slider', {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      centeredSlides: true,
+      pagination: {
+        el: ".materials__dots",
+        bulletClass: "materials__dot",
+        bulletActiveClass: "materials__dot--active",
+        clickable: true,
+      }
+    });
+  };
 });
